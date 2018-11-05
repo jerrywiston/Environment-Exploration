89d0b8184546755895c47f467cf3f6ee3640387b
jerrywiston <dreamfantasy0@gmail.com>
2018-11-05 21:14:42 +0800

    change some parameters

--- test2.py
+++ test2.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # SensorSize, StartAngle, EndAngle, MaxDist, Velocity, Angular
     bot_param = [240,-30.0, 210.0, 150.0, 6.0, 6.0]
     bot_pos = np.array([100.0, 100.0, 90.0])
-    motion = SimpleMotionModel(1.0, 1.0, 0.5)
+    motion = SimpleMotionModel(0.5, 0.5, 0.3)
     env = SingleBotLaser2D(bot_pos, bot_param, 'Image/map_large.png', motion)
 
     # Initialize GridMap
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     cv2.imshow('map',mimg)
 
     # Initialize Particle
-    total_particle = 50
+    total_particle = 30
     pf = ParticleFilter(bot_pos.copy(), bot_param, copy.deepcopy(m), motion, total_particle)
     sensor_data_rec = sensor_data.copy()
     pimg = AdaptiveGetMap(pf.particle_list[0].gmap)
